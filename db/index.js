4ed7ec965d70181d8b3b2dcdf1145b1ecf431931
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-10-30 16:16:08 +0700

    Change database config from localhost Server to Heroku Server

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: '123456789',
-    database: 'retro-react-db',
-    host: 'localhost',
+    user: 'rwizknzfpssofl',
+    password: '6702737f3b05e6e7f4fbe0a2b7e510a1784555fa2a2edacf287be2d49e105b96',
+    database: 'de2ffqjr7iv2je',
+    host: 'ec2-52-44-235-121.compute-1.amazonaws.com',
     port: 5432
 });
 
